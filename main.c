61462d15ea8ae3037e0fffca1cead03fcb3eaf1f
ivanranea <ivan_ranea@hotmail.com>
2020-07-21 16:06:12 -0300

    Paso de variable dineroTotal como puntero

--- main.c
+++ main.c
@@ -32,7 +32,7 @@
 #define A_PRIMER_COLUMNA 1
 #define A_SEGUNDA_COLUMNA 2
 #define A_TERCERA_COLUMNA 3
-int dineroTotalJugador = 200; //Dinero inicial del jugador, no se discriminan las fichas.
+
 const int numerosRojos[]={1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
 const int numerosNegros[]={2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35};
 const int columna1[]={1,4,7,10,13,16,19,22,25,28,31,34};
@@ -313,7 +313,7 @@ void imprimirApuesta(Apuesta a){
 //    return 0;
 //}
 
-int registrarMontoFichas(int dineroTotalJugador){
+int registrarMontoFichas(int *dineroTotalJugador){
     // Pasar como parametro dineroTotalJugador
     int fichasTemp = 0;
     int fichasApostadas = 0;
@@ -321,7 +321,7 @@ int registrarMontoFichas(int dineroTotalJugador){
     while(TRUE) // Inicio ingreso de fichas para la apuesta
     {
 
-        printf("\nFichas disponibles: $1, $2, $5, $10, $50, $100\nDinero disponible: %d\nCuando desee finalizar la apuesta ingrese 0\nIngrese la ficha que desea apostar: ", dineroTotalJugador);
+        printf("\nFichas disponibles: $1, $2, $5, $10, $50, $100\nDinero disponible: %d\nCuando desee finalizar la apuesta ingrese 0\nIngrese la ficha que desea apostar: ", *dineroTotalJugador);
         scanf("%d", &fichasTemp);
 
         while(fichasTemp!=0 && fichasTemp!=1 && fichasTemp!=2 && fichasTemp!=5 && fichasTemp!=10 && fichasTemp!=50 && fichasTemp!=100)
@@ -350,18 +350,18 @@ int registrarMontoFichas(int dineroTotalJugador){
         }else
         {
 
-            if(fichasTemp>dineroTotalJugador)
+            if(fichasTemp>(*dineroTotalJugador))
             {
 
-                printf("\nEl valor de las fichas excede su dinero disponible ($%d),\nvuelva a ingresar las fichas.\n", dineroTotalJugador);
-                dineroTotalJugador += fichasApostadas;
+                printf("\nEl valor de las fichas excede su dinero disponible ($%d),\nvuelva a ingresar las fichas.\n", *dineroTotalJugador);
+                *dineroTotalJugador += fichasApostadas;
                 fichasApostadas = 0;
                 fichasTemp = 0;
 
 
             }else
             {
-                dineroTotalJugador -= fichasTemp;
+                *dineroTotalJugador -= fichasTemp;
                 fichasApostadas += fichasTemp;
                 printf("\nApuesta total: $%d\n", fichasApostadas);
 
@@ -373,7 +373,7 @@ int registrarMontoFichas(int dineroTotalJugador){
 }
 
 
-Apuesta registrarApuestaPlenos(){
+Apuesta registrarApuestaPlenos(int *dineroTotalJugador){
     int valor;
     printf("Ingrese el número al cual desea apostar (0 al 36): ");
     scanf("%d", &valor);
@@ -389,7 +389,7 @@ Apuesta registrarApuestaPlenos(){
     return a;
 }
 
-Apuesta registrarApuestaDocenas(){
+Apuesta registrarApuestaDocenas(int *dineroTotalJugador){
     int valor;
     printf("Ingrese a qué docena desea apostar:\n%d.Primera docena\n%d.Segunda docena\n%d.Tercera docena\n",
             A_PRIMER_DOCENA, A_SEGUNDA_DOCENA, A_TERCERA_DOCENA);
@@ -406,7 +406,7 @@ Apuesta registrarApuestaDocenas(){
     return a;
 }
 
-Apuesta registrarApuestaFalta(){
+Apuesta registrarApuestaFalta(int *dineroTotalJugador){
 
     Apuesta a;
     a.tipo = A_FALTA;
@@ -415,7 +415,7 @@ Apuesta registrarApuestaFalta(){
     return a;
 }
 
-Apuesta registrarApuestaPasa(){
+Apuesta registrarApuestaPasa(int *dineroTotalJugador){
 
     Apuesta a;
     a.tipo = A_PASA;
@@ -425,7 +425,7 @@ Apuesta registrarApuestaPasa(){
 }
 
 
-Apuesta registrarApuestaColor(){
+Apuesta registrarApuestaColor(int *dineroTotalJugador){
     int valor;
     printf("Ingrese el color al desea apostar:\n1.Rojo\n2.Negro\n");
     scanf("%d", &valor);
@@ -442,7 +442,7 @@ Apuesta registrarApuestaColor(){
     return a;
 }
 
-Apuesta registrarApuestaParidad(){
+Apuesta registrarApuestaParidad(int *dineroTotalJugador){
     int valor;
     printf("Ingrese si desea apostar a los números pares o impares:\n1.Par\n2.Impar\n");
     scanf("%d", &valor);
@@ -459,7 +459,7 @@ Apuesta registrarApuestaParidad(){
     return a;
 }
 
-Apuesta registrarApuestaColumna(){
+Apuesta registrarApuestaColumna(int *dineroTotalJugador){
     int valor;
     printf("Ingrese el tipo de columna a la que desea apostar:\n1.Columna del 1\n2.Columna del 2\n3.Columna del 3\n");
     scanf("%d", &valor);
@@ -479,9 +479,10 @@ Apuesta registrarApuestaColumna(){
 
 
 
-void preguntarApuestasAlUsuario(Apuesta *apuestas, int nApuestas){
+void preguntarApuestasAlUsuario(Apuesta *apuestas, int nApuestas, int *dineroTotalJugador){
+    int i;
 
-    for(int i=0; i<nApuestas; i++) // Comienzo for apuestas
+    for(i=0; i<nApuestas; i++) // Comienzo for apuestas
     {
         int tipoApuestas;
         printf("\n1.Pleno\n2.Docenas\n3.Falta (1 al 18)\n4.Pasa (19 al 36)\n5.Color\n6.Pares o Impares\n7.Columnas\n");
@@ -497,33 +498,33 @@ void preguntarApuestasAlUsuario(Apuesta *apuestas, int nApuestas){
         switch(tipoApuestas) //Inicio Swith Tipo Apuestas
         {
             case A_PLENO: //Plenos
-                apuestas[i] = registrarApuestaPlenos();
+                apuestas[i] = registrarApuestaPlenos(dineroTotalJugador);
                 break;
 
             case A_DOCENAS: //Docenas
-                apuestas[i] = registrarApuestaDocenas();
+                apuestas[i] = registrarApuestaDocenas(dineroTotalJugador);
                 break;
 
             case A_FALTA:  // Falta
-                apuestas[i] = registrarApuestaFalta();
+                apuestas[i] = registrarApuestaFalta(dineroTotalJugador);
                 break;
 
             case A_PASA:  // Pasa
-                apuestas[i] = registrarApuestaPasa();
+                apuestas[i] = registrarApuestaPasa(dineroTotalJugador);
                 break;
 
             case A_COLOR: // Color
-                apuestas[i] = registrarApuestaColor(); // TODO Mover a una funcion - Lore
+                apuestas[i] = registrarApuestaColor(dineroTotalJugador);
                 break;
 
             case A_PARIDAD: // Par o Impar
-                apuestas[i] = registrarApuestaParidad();
-                // TODO Mover a una funcion - Lore
+                apuestas[i] = registrarApuestaParidad(dineroTotalJugador);
+
                 break;
 
             case A_COLUMNA: // Columnas =
-                apuestas[i] = registrarApuestaColumna();
-                // TODO Mover a una funcion - Lore
+                apuestas[i] = registrarApuestaColumna(dineroTotalJugador);
+
                 break;
 
             default: break;
@@ -536,11 +537,12 @@ int main(){
     // Esta funcion es un test para preguntarApuestasAlUsuario()
     int dineroTotalJugador = 1000;
     int nApuestas;
+    int i;
     printf("Elija la cantidad de apuestas: ");
     scanf("%d", &nApuestas);
     Apuesta apuestas[nApuestas];
-    preguntarApuestasAlUsuario(apuestas, nApuestas);
-    for (int i=0;i<nApuestas;i++){
+    preguntarApuestasAlUsuario(apuestas, nApuestas, &dineroTotalJugador);
+    for (i=0;i<nApuestas;i++){
         imprimirApuesta(apuestas[i]);
     }
     return 0;
